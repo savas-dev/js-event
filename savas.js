387c77ad0591d37a35109c6e3074c26adeaedcea
savas-dev <138906329+savas-dev@users.noreply.github.com>
2023-08-06 11:16:05 +0300

    project files

--- savas.js
+++ savas.js
@@ -3,13 +3,16 @@ const remainingForm = document.getElementById("remainingForm");
 const timeDiv = document.getElementById("timeDiv");
 const datePicker = document.getElementById("datePicker");
 const timeSpans = document.querySelectorAll("span");
-const resetButton  = document.getElementById('resetButton');
+const resetButton = document.getElementById("resetButton");
+const timeNow = document.getElementById("timeNow");
+const completeButton = document.getElementById('completeButton');
 
 const today = new Date().toISOString().split("T")[0];
 
 let chosenDate = "";
 let currentDateValue = new Date().getTime();
 let currentTimeInterval;
+let localStorageTime;
 
 const second = 1000;
 const minute = second * 60;
@@ -29,24 +32,44 @@ function UpdateDom() {
     const seconds = Math.floor((betweenDatte % minute) / second);
 
     remainingDiv.hidden = true;
-    timeDiv.hidden = false;
 
-    timeSpans[0].textContent = `${days}`;
-    timeSpans[1].textContent = `${hours}`;
-    timeSpans[2].textContent = `${minutes}`;
-    timeSpans[3].textContent = `${seconds}`;
+    if (betweenDatte < 0) {
+      timeDiv.hidden = true;
+      clearInterval(currentTimeInterval);
+      timeNow.hidden = false;
+    } else {
+      
+      timeDiv.hidden = false;
+
+      timeSpans[0].textContent = `${days}`;
+      timeSpans[1].textContent = `${hours}`;
+      timeSpans[2].textContent = `${minutes}`;
+      timeSpans[3].textContent = `${seconds}`;
+    }
   }, 1000);
 }
 
-function ResetTime(){
-    clearInterval(currentTimeInterval);
-    timeDiv.hidden = true;
+function ResetTime() {
+  clearInterval(currentTimeInterval);
+  timeDiv.hidden = true;
+  remainingDiv.hidden = false;
+  localStorage.removeItem('time');
+
+}
+
+function CalculateAgain(){
+    timeNow.hidden = true;
     remainingDiv.hidden = false;
 }
 
 remainingForm.addEventListener("submit", (e) => {
   e.preventDefault();
   chosenDate = remainingForm.date.value;
+
+  localStorageTime = {
+    date: chosenDate,
+  };
+  localStorage.setItem('time', JSON.stringify(localStorageTime));
 
   if (chosenDate == "") {
     alert("Please choose a valid date");
@@ -56,4 +79,19 @@ remainingForm.addEventListener("submit", (e) => {
   }
 });
 
-resetButton.addEventListener('click', ResetTime);
+function RefreshTime(){
+    if(localStorage.getItem('time')){
+        remainingDiv.hidden = true;
+        localStorageTime = JSON.parse(localStorage.getItem('time'));
+        chosenDate = localStorageTime.date;
+        currentDateValue = new Date(chosenDate).getTime();
+        
+        UpdateDom();
+    }
+}
+
+resetButton.addEventListener("click", ResetTime);
+
+completeButton.addEventListener('click', CalculateAgain);
+
+RefreshTime();
